e47dab0d4be93441695d1d8877d433f9e516e09f
pallobbiswas <pallobbiswas326@gmail.com>
2022-06-15 14:46:55 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
-import Footer from './components/Footer/Footer';
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div>
-      
-     <h1>hello guys</h1>
-     <h1>Nasir</h1>
-     <Footer></Footer>
+      <Navbar></Navbar>
+      <h1>hello guys</h1>
+      <h1>Nasir</h1>
+      <Footer></Footer>
     </div>
   );
 }
