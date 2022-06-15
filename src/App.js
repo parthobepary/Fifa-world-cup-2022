e73a7171668efa0f00b0c2de7e945b80b3a34b15
nasirdiu <nasir.joypurhat@gmail.com>
2022-06-15 14:40:59 +0600

    a

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div>
+      
      <h1>hello guys</h1>
      <h1>Nasir</h1>
+     <Footer></Footer>
     </div>
   );
 }
