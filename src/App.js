886a8f7bb10e5589b77d9c253db0423970a95db3
nasirdiu <nasir.joypurhat@gmail.com>
2022-06-16 14:53:03 +0600

    live

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import Banner from "./Pages/Home/Banner/Banner";
+import LiveStream from "./Pages/Home/LiveStream/LiveStream";
 import Studium from "./Pages/Home/Studium/Studium";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Navbar></Navbar>
       <Banner></Banner>
+      <LiveStream></LiveStream>
       <Studium></Studium>
       <Footer></Footer>
     </div>
