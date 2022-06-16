7c435e8bebdb49a01decb2a98befc133291bb40f
pallobbiswas <pallobbiswas326@gmail.com>
2022-06-16 16:14:46 +0600

    timer done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import Banner from "./Pages/Home/Banner/Banner";
 import Studium from "./Pages/Home/Studium/Studium";
+import Timer from "./Pages/Home/Timer";
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
       <Banner></Banner>
+      <Timer></Timer>
       <Studium></Studium>
       <Footer></Footer>
     </div>
