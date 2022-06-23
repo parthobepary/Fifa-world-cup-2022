e58126f2eee903aaedec63b23366cc497ed93c6b
pallobbiswas <pallobbiswas326@gmail.com>
2022-06-23 15:36:15 +0600

    width fixed done

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,9 @@
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div>
-      <div class="navbar bg-base-100">
+      <div class="navbar bg-base-100 px-10">
         <div class="navbar-start">
           <div class="dropdown">
             <label tabindex="0" class="btn btn-ghost lg:hidden">
@@ -28,61 +29,17 @@ const Navbar = () => {
               <li>
                 <a>Item 1</a>
               </li>
-              <li tabindex="0">
-                <a class="justify-between">
-                  Parent
-                  <svg
-                    class="fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                  </svg>
-                </a>
-                <ul class="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
               <li>
                 <a>Item 3</a>
               </li>
             </ul>
           </div>
-          <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+          <p class=" normal-case text-xl">Fifa world cup 2022</p>
         </div>
-        <div class="navbar-center hidden lg:flex">
+        <div class="navbar-center hidden lg:flex navbar-end">
           <ul class="menu menu-horizontal p-0">
             <li>
               <a>Item 1</a>
-            </li>
-            <li tabindex="0">
-              <a>
-                Parent
-                <svg
-                  class="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                </svg>
-              </a>
-              <ul class="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
             </li>
             <li>
               <a>Item 3</a>
@@ -90,7 +47,7 @@ const Navbar = () => {
           </ul>
         </div>
         <div class="navbar-end">
-          <a class="btn">Get started</a>
+          <Link to="/login">Login</Link>
         </div>
       </div>
     </div>
